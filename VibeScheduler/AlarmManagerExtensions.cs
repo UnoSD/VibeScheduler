4a05654789bbe6ac242e1200032a62fad4658e17
UnoSD <unosd.dev@gmail.com>
2017-09-16 00:08:44 +0100

    Adding different alarm clock set invocation

--- VibeScheduler/AlarmManagerExtensions.cs
+++ VibeScheduler/AlarmManagerExtensions.cs
@@ -5,7 +5,10 @@ namespace VibeScheduler
 {
     public static class AlarmManagerExtensions
     {
-        public static void ScheduleIntent(this AlarmManager manager, DateTime dateTime, PendingIntent intent) => 
+        public static void ScheduleIntentExactAllowIdle(this AlarmManager manager, DateTime dateTime, PendingIntent intent) =>
             manager.SetExactAndAllowWhileIdle(AlarmType.RtcWakeup, dateTime.ToUnix(), intent);
+
+        public static void ScheduleIntent(this AlarmManager manager, DateTime dateTime, PendingIntent intent) =>
+            manager.SetAlarmClock(new AlarmManager.AlarmClockInfo(dateTime.ToUnix(), intent), intent);
     }
 }
